98fc35263eea317a3f7c0cb6d5a2d7a0c5cb952f
Desu Desu <102465369+neuroliptica@users.noreply.github.com>
2023-04-01 13:10:18 +0300

    Small rework again.

--- env/cookies.go
+++ env/cookies.go
@@ -13,8 +13,24 @@ import (
 // Cast proto.NetworkCookie to http.Cookie.
 func protoToHttp(pCookies []*proto.NetworkCookie) []*http.Cookie {
 	cookies := make([]*http.Cookie, 0)
+	samesite := map[string]http.SameSite{
+		"None":   3,
+		"Strict": 2,
+		"Lax":    1,
+	}
 	for i := range pCookies {
-		cookie := http.Cookie{Name: pCookies[i].Name, Value: pCookies[i].Value}
+		cookie := http.Cookie{
+			Name:     pCookies[i].Name,
+			Value:    pCookies[i].Value,
+			HttpOnly: pCookies[i].HTTPOnly,
+			Secure:   pCookies[i].Secure,
+			Domain:   pCookies[i].Domain,
+			Path:     pCookies[i].Path,
+			Expires:  pCookies[i].Expires.Time(),
+		}
+		if val, ok := samesite[string(pCookies[i].SameSite)]; ok {
+			cookie.SameSite = val
+		}
 		cookies = append(cookies, &cookie)
 	}
 	return cookies
@@ -54,14 +70,18 @@ func GetHeaders(url string, wait time.Duration) ([]*http.Cookie, map[string]Head
 	//for key, value := range e.Request.Headers {
 	//	headers[key] = Header(value.String())
 	//}
-	headers["Accept"] = Header("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	headers["Accept"] = Header("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
 	headers["Accept-Encoding"] = Header("gzip, deflate, br")
-	headers["Accept-Language"] = Header("en")
+	headers["Accept-Language"] = Header("en-US,en;q=0.5")
+	headers["DNT"] = Header("1")
+	//headers["TE"] = Header("trailers")
 	headers["Sec-Fetch-Dest"] = Header("document")
 	headers["Sec-Fetch-Mode"] = Header("navigate")
 	headers["Sec-Fetch-Site"] = Header("none")
 	headers["Sec-Fetch-User"] = Header("?1")
+	//headers["Connection"] = Header("keep-alive")
 	headers["Upgrade-Insecure-Requests"] = Header("1")
 	headers["User-Agent"] = Header("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+
 	return protoToHttp(cookies), headers
 }
--- main.go
+++ main.go
@@ -4,27 +4,25 @@ import (
 	"dollwipe/engine"
 	"dollwipe/env"
 	"dollwipe/network"
+	"fmt"
 	"log"
 	"math"
 	"os"
 	"time"
 )
 
-// Isolated goroutine for logging.
-func logger(lenv *env.Env) {
-	for v := range lenv.Logger {
-		log.Println(v)
+func logger(messages <-chan string) {
+	for msg := range messages {
+		log.Println(msg)
 	}
 }
 
-// Isolated bad proxies filter.
-func filter(lenv *env.Env, posts map[string]*engine.Post) {
-	for v := range lenv.Filter {
-		delete(posts, v)
+func filter(bad <-chan string, posts map[string]*engine.Post) {
+	for proxy := range bad {
+		delete(posts, proxy)
 	}
 }
 
-// Isolated goroutine for statistics.
 func counter(lenv *env.Env) {
 	// TODO: should use mutex to prevent shit increment btw.
 	for v := range lenv.Status {
@@ -42,7 +40,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	go logger(lenv)
+	go logger(lenv.Logger)
 	go counter(lenv)
 
 	// Init posts. Also if we do not use proxy, "localhost" will be count as a proxy in proxy map.
@@ -66,8 +64,8 @@ func main() {
 			} else {
 				Posts[v.Address()] = v.Post()
 			}
-			log.Printf("OK: %3d; FAIL: %3d",
-				len(Posts), failed)
+			lenv.Logger <- fmt.Sprintf(
+				"OK: %3d; FAIL: %3d", len(Posts), failed)
 			if failed+len(Posts) == len(lenv.Proxies) {
 				done <- true
 				return
@@ -82,9 +80,10 @@ func main() {
 	<-initDone
 
 	if lenv.UseProxy {
-		log.Printf("проксей инициализировано - %d.", len(lenv.Proxies))
+		lenv.Logger <- fmt.Sprintf(
+			"проксей инициализировано - %d.", len(lenv.Proxies))
 	}
-	go filter(lenv, Posts)
+	go filter(lenv.Filter, Posts)
 
 	for i := uint64(0); i < lenv.Iters; i++ {
 		var (
@@ -101,17 +100,21 @@ func main() {
 				used++
 			}
 		}
-		log.Printf("итерация %d; постов будет отправлено - %d; перерыв - %d сек.", i+1, used, lenv.Timeout)
+		lenv.Logger <- fmt.Sprintf(
+			"итерация %d; постов будет отправлено - %d; перерыв - %d сек.",
+			i+1, used, lenv.Timeout)
 		for j := uint64(0); j < used; j++ {
 			go engine.RunPost(Posts[alive[j]])
 		}
 		for uint64(lenv.PostsOk+lenv.PostsFailed) != used {
 			time.Sleep(time.Second * 2)
 		}
-		log.Printf("Успешно отправлено - %d; всего отправлено - %d.", lenv.PostsOk, lenv.PostsOk+lenv.PostsFailed)
+		lenv.Logger <- fmt.Sprintf(
+			"Успешно отправлено - %d; всего отправлено - %d.",
+			lenv.PostsOk, lenv.PostsOk+lenv.PostsFailed)
 		lenv.PostsOk, lenv.PostsFailed = 0, 0
 		if len(Posts) == 0 {
-			log.Println("все проксичи умерли, помянем.")
+			lenv.Logger <- fmt.Sprintf("все проксичи умерли, помянем.")
 			os.Exit(0)
 		}
 		if i+1 != lenv.Iters {
